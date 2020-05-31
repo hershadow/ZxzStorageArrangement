77a4d7a219153954b60cadeb5bdc2686b9e34f3d
hershadow <hershadow@users.noreply.github.com>
2020-05-31 18:01:25 +0800

    solve the visible
    修复firstFragment的搜索栏只能收一半的BUG

--- app/src/main/java/com/zxzlst/zxzstoragearrangement/ui/mainfragment/FirstFragment.kt
+++ app/src/main/java/com/zxzlst/zxzstoragearrangement/ui/mainfragment/FirstFragment.kt
@@ -1,27 +1,29 @@
 package com.zxzlst.zxzstoragearrangement.ui.mainfragment
 
-import android.app.Activity
-import android.content.Context
+import android.os.Build
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.appbar.AppBarLayout
 import com.zxzlst.zxzstoragearrangement.MainActivity
 
 import com.zxzlst.zxzstoragearrangement.R
 import com.zxzlst.zxzstoragearrangement.Repository
-import com.zxzlst.zxzstoragearrangement.insertmodule.CameraActivity
 import com.zxzlst.zxzstoragearrangement.logic.dao.createItem
 import com.zxzlst.zxzstoragearrangement.ui.adapter.FirstAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.first_fragment.*
+import java.lang.Exception
 
 class FirstFragment : Fragment() {
 
@@ -44,27 +46,54 @@ class FirstFragment : Fragment() {
         viewModel = ViewModelProviders.of(this).get(FirstViewModel::class.java)
         (requireActivity() as MainActivity).supportActionBar?.hide()
 
+
+
+        //appbar向上收纳的时候隐藏
+        firstFragment_AppBarLayout.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener{
+            override fun onOffsetChanged(p0: AppBarLayout?, p1: Int) {
+                if (p0 != null){
+                    if (p1 + p0.height == 0){
+                        p0.visibility = View.INVISIBLE
+                    }else{
+                        p0.visibility = View.VISIBLE
+                    }
+                }
+            }
+        })
+
+
+
+
         //recyclerView
         val layoutManager = LinearLayoutManager(requireActivity())
         firstFragment_researchRecyclerView.layoutManager = layoutManager
         adapter = FirstAdapter(this,viewModel.resultList)
         firstFragment_researchRecyclerView.adapter = adapter
         firstFragment_searchEditText.addTextChangedListener{
-            if (it != null){
-                val content = it.toString()
+            val content = it.toString()
+            if (content != ""){
                 viewModel.searchForResult(content)
+                Log.d("zxzzxzzxz","1")
             }else{
                 firstFragment_researchRecyclerView.visibility = View.GONE
                 viewModel.resultList.clear()
                 adapter.notifyDataSetChanged()
+                Log.d("zxzzxzzxz","2")
             }
         }
         viewModel.resultLiveData.observe(viewLifecycleOwner, Observer {
-            if (it != null){
+            if (it.isNotEmpty()){
                 firstFragment_researchRecyclerView.visibility = View.VISIBLE
                 viewModel.resultList.clear()
                 viewModel.resultList.addAll(it)
                 adapter.notifyDataSetChanged()
+                Log.d("zxzzxzzxz","3")
+            }else{
+                viewModel.resultList.clear()
+                adapter.notifyDataSetChanged()
+                firstFragment_researchRecyclerView.visibility = View.GONE
+                Toast.makeText(requireContext(),"无匹配物品",Toast.LENGTH_SHORT).show()
+                Log.d("zxzzxzzxz","4")
             }
         })
 
